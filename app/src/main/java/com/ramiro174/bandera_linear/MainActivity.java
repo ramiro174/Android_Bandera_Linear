84c379c2047f400f57464b39c78763930a93cea2
Ramiro <ramiro174@gmail.com>
2022-09-29 13:13:23 -0500

    add archivos iniciales

---
+++ app/src/main/java/com/ramiro174/bandera_linear/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ramiro174.bandera_linear;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
